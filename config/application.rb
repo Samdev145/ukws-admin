8f2762c1ece7320faf54f2d546073f0ddf495eff
= <=>
2022-10-21 11:59:16 +0100

    fix: make smtp timeout configurable

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module UkwsAdmin
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.action_mailer.smtp_timeout = ENV['SMTP_TIMEOUT'].to_i
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
