5c8b0a1fb86502f82dd09e668625e8b4f2235cbb
= <=>
2022-10-25 14:14:08 +0100

    feat: show flash messages in views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+  def flash_style(type)
+    case type
+      when 'notice' then 'primary'
+      when 'alert' then 'danger'
+      when 'warning' then 'warning'
+      when 'info' then 'info'
+      when 'success' then 'success'
+    end
+  end
 end
