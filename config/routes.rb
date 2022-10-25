f55d3cda2e0c4d1eee0a087046ada7fdd1c201d8
= <=>
2022-10-25 13:58:27 +0100

    fix: remove redundant route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   match '/404', to: 'errors#not_found', via: :all, as: :not_found
   match '/500', to: 'errors#internal_server_error', via: :all
 
-  get 'products/index'
   root 'dashboard#index'
 
   get 'auth/:provider/callback', to: 'sessions#create'
