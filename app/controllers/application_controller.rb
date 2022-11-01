2db01e8ea609d654394b929e9a81dfdcf4dbcba6
= <=>
2022-11-01 16:05:47 +0000

    feat: Add method to check if authenticated to calender client

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,13 @@ class ApplicationController < ActionController::Base
     redirect_to :login if session[CRM::Provider].nil? || crm_client.invalid_session?
   end
 
+  def authenticate_calendar_client_user
+    if session[CALENDAR_CLIENT::Provider].nil?
+      flash[:alert] = "Sign in using google if you want to be able to book appointments"
+      redirect_to :login
+    end
+  end
+
   private
 
   def calendar_client
