fd5f9c353a6e2f00b19b870e7217b93864b7f118
= <=>
2022-11-08 11:20:30 +0000

    fix: ensure product search is case insensitive

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ApplicationRecord
   validates :name, :record_type, :photos, presence: true
 
   def self.main_photo_for(product_name)
-    record = find_by_name(product_name)
+    record = where('lower(name) = ?', product_name.downcase).first
     return nil if record.nil?
 
     record.find_attachment_by_filename(:main_photo)
