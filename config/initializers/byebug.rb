ec34b7ecbf6a4c4ea973a56fcce1537b4c797e3c
= <=>
2022-11-10 14:05:21 +0000

    fix: enable byebug to work after running workers locally

--- config/initializers/byebug.rb
+++ config/initializers/byebug.rb
@@ -4,7 +4,7 @@ if Rails.env.development?
   require 'byebug/core'
   begin
     Byebug.start_server 'localhost', ENV.fetch('BYEBUG_SERVER_PORT', 8989).to_i
-  rescue Errno::EADDRINUSE
+  rescue Errno::EADDRINUSE, Errno::EADDRNOTAVAIL
     Rails.logger.debug 'Byebug server already running'
   end
 end
