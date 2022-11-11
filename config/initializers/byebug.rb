6bb1f93980fcd06129f38b91adf59867dbf0fc5a
= <=>
2022-11-11 14:56:45 +0000

    fix: Ensure byebug connects to the correct process

--- config/initializers/byebug.rb
+++ config/initializers/byebug.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if Rails.env.development?
+if Rails.env.development? && ENV['MAIN_THREAD'] == 'true'
   require 'byebug/core'
   begin
     Byebug.start_server 'localhost', ENV.fetch('BYEBUG_SERVER_PORT', 8989).to_i
