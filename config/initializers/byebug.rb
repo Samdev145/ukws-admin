c33b330038e84b0ae0742e1590d8da34bd34f068
= <=>
2022-11-15 19:57:58 +0000

    feat: allow byebug against both web and worker processes

--- config/initializers/byebug.rb
+++ config/initializers/byebug.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if Rails.env.development? && ENV['MAIN_THREAD'] == 'true'
+if Rails.env.development? && ENV['THREAD_TYPE'] == ENV['BYEBUG_THREAD']
   require 'byebug/core'
   begin
     Byebug.start_server 'localhost', ENV.fetch('BYEBUG_SERVER_PORT', 8989).to_i
