a907c741a3719534c3911d19690f9af1da951b3d
= <=>
2022-11-22 09:27:13 +0000

    fix: restrict scopes used to access crm data

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,8 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :zoho, ENV.fetch('ZOHO_API_KEY'), ENV.fetch('ZOHO_SHARED_SECRET'),
-           scope: 'ZohoCRM.org.ALL,ZohoCRM.modules.ALL,ZohoSearch.securesearch.READ,ZohoCRM.coql.READ'
-  provider :google_oauth2, ENV.fetch('GOOGLE_CLIENT_ID'), ENV.fetch('GOOGLE_CLIENT_SECRET'), scope: 'https://www.googleapis.com/auth/calendar,email'
+           scope: 'ZohoCRM.org.READ,ZohoCRM.modules.leads.READ,ZohoCRM.modules.contacts.READ,ZohoCRM.coql.READ'
+
+  provider :google_oauth2, ENV.fetch('GOOGLE_CLIENT_ID'), ENV.fetch('GOOGLE_CLIENT_SECRET'),
+           scope: 'https://www.googleapis.com/auth/calendar,email'
 end
